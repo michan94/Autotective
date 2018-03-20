5e06e4555405a9a76223dd9001e900ec3248ae96
garfield041 <mitchan_94@hotmail.com>
2018-03-20 15:36:40 -0700

    Main class added

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.sql.*;
+
+public class Main {
+    //JDBC drive name and database URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/Autotective";
+
+    //Database credentials
+    static final String USER = "tester";
+    static final String Pass = "setting";
+
+        public static void main (String args[]) {
+            Connection conn = null;
+            Statement stmt = null;
+        }
+
+
+}
