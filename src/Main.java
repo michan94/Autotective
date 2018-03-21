f8b4e78609449ad78fa18e721cd7a5b37e7359c6
garfield041 <mitchan_94@hotmail.com>
2018-03-20 18:26:14 -0700

    Completed main

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,68 @@ public class Main {
 
     //Database credentials
     static final String USER = "tester";
-    static final String Pass = "setting";
+    static final String PASS = "setting";
 
         public static void main (String args[]) {
             Connection conn = null;
             Statement stmt = null;
+            try {
+                //Register JDBC Driver
+                Class.forName(JDBC_DRIVER);
+
+                //Open Connection
+                System.out.println("Connecting to database...");
+                conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+                //Query Execution
+                System.out.println("Creating statement...");
+                stmt = conn.createStatement();
+                String sql;
+                sql = "SELECT id, first last, age FROM Employees"; //Change these to our project
+                ResultSet rs = stmt.executeQuery(sql);
+
+                //Extract data from result set
+                while(rs.next()) {
+                //Retrieve by column name
+                    int id = rs.getInt("id");
+                    int age = rs.getInt("age");
+                    String first = rs.getString("first");
+                    String last = rs.getString("last");
+
+                    //Display Values
+                    System.out.println("ID: " + id);
+                    System.out.println(", Age: " + age);
+                    System.out.println(", First: " + first);
+                    System.out.println(", Last: " + last);
+                }
+
+                //Clean-up environment
+                rs.close();
+                stmt.close();
+                conn.close();
+            }catch (SQLException se) {
+                //Handle errors for JDBC
+                se.printStackTrace();
+            }catch (Exception e) {
+                //Handle errors for Class.forName
+                e.printStackTrace();
+            }finally {
+                //finally block used to close resources
+                try {
+                    if (stmt != null) {
+                        stmt.close();
+                    }
+                }catch (SQLException se2) {
+                    //Nothing we can do
+                }
+                try {
+                    if (conn != null) {
+                        conn.close();
+                    }
+                }catch (SQLException se) {
+                    se.printStackTrace();
+                }// end finally try
+            }//end try
+            System.out.println("Goodbye!");
         }
-
-
 }
